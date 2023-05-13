c405f019b7c4b5413e6bd9fb00c053e1382447cf
GiovanniBSDuarte <giovannibsduarte@gmail.com>
2023-05-13 13:26:28 -0300

    cliente especialista e regular file e construtor

---
+++ codigo/app/src/main/java/business/ClienteEspecialista.java
@@ -0,0 +1,10 @@
+package business;
+
+public class ClienteEspecialista extends Cliente{
+
+    public ClienteEspecialista(String nomeCompleto, String nomeDeUsuario, String senha) {
+        super(nomeCompleto, nomeDeUsuario, senha);
+        
+    }
+    
+}
---
+++ codigo/app/src/main/java/business/ClienteRegular.java
@@ -0,0 +1,9 @@
+package business;
+public class ClienteRegular extends Cliente{
+
+    public ClienteRegular(String nomeCompleto, String nomeDeUsuario, String senha) {
+        super(nomeCompleto, nomeDeUsuario, senha);
+        
+    }
+    
+}
