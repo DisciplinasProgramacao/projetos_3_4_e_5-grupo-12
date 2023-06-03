8d809d31e27cee2e29d9f9f92a1db66de9830074
Drumond <drumond.work@gmail.com>
2023-06-03 19:05:49 -0300

    class Lancavel criada

---
+++ codigo/app/src/main/java/business/Lancavel.java
@@ -0,0 +1,11 @@
+package business;
+
+public interface Lancavel {
+
+    public boolean isLancamento();
+
+    public void setLancamento(boolean lancamento);
+
+    public boolean podeAssistir(ClienteProfissional cliente);
+    
+}
