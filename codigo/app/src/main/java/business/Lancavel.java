9b6eeccccb837cb16ae1d0e8f6c5314793aba79d
Drumond <drumond.work@gmail.com>
2023-06-06 15:39:01 -0300

    lancavel markup interface

--- codigo/app/src/main/java/business/Lancavel.java
+++ codigo/app/src/main/java/business/Lancavel.java
@@ -2,10 +2,5 @@ package business;
 
 public interface Lancavel {
 
-    //public boolean isLancamento();
-
-    public void setLancamento(Lancavel lancamento);
-
-    //public boolean podeAssistir(ClienteProfissional cliente);
     
 }
